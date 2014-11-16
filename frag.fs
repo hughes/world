1659c1596fb810cecb37363b4702fd4e90f0428d
Matt Hughes <hughesm02@gmail.com>
2014-11-16 18:57:28 -0500

    tidyup

--- frag.fs
+++
@@ -1,9 +0,0 @@
-uniform float time;
-#ifdef GL_ES
-precision highp float;
-#endif
-
-void main()
-{
-    gl_FragColor = vec4(1.0,1.0,1.0,1.0);
-}
--- main.js
+++ main.js
@@ -6,7 +6,6 @@ var WIDTH = window.innerWidth,
     NEAR = 0.1,
     FAR = 10000;
 
-var stats = new Stats();
 var renderer = new THREE.WebGLRenderer();
 var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 var scene = new THREE.Scene();
@@ -25,60 +24,27 @@ function onWindowResize(){
 
 var container = document.getElementById('container');
 container.appendChild(renderer.domElement);
-container.appendChild(stats.domElement);
-
-var vShader = document.getElementById('vert').innerText,
-    fShader = document.getElementById('frag').innerText;
 
 var uniforms = {
-    time: { type: 'f', value: 0.0 }
-};
-
-var material = new THREE.ShaderMaterial({
-    uniforms: uniforms,
-    vertexShader: vShader,
-    fragmentShader: fShader
-});
-
-// var geometry, lineData, pointData, x, y;
-// coastline.forEach(function (lineData) {
-//     geometry = new THREE.Geometry();
-//     lineData.forEach(function (pointData) {
-//         x = pointData[0];
-//         y = pointData[1];
-//         geometry.vertices.push(new THREE.Vector3(x, y, 0));
-//     });
-//     scene.add(new THREE.Line(geometry, material));
-// });
-
-var uniforms2 = {
-    // time: { type: 'f', value: 0.0 },
+    time: { type: 'f', value: 0.0 },
     texture1: { type: 't', value: THREE.ImageUtils.loadTexture('./ocean_dist_resize.png') },
     cutoff: { type: 'f', value: 0.456887065393 }
 };
 
+uniforms.texture1.value.wrapS = uniforms.texture1.value.wrapT = THREE.RepeatWrapping;
+
 var textVShader = document.getElementById('texturedVert').innerText,
     textFShader = document.getElementById('texturedFrag').innerText;
 
-var geometry2   = new THREE.SphereGeometry(80, 32, 32);
-var material2  = new THREE.ShaderMaterial({
-    uniforms: uniforms2,
+var geometry   = new THREE.SphereGeometry(100, 128, 64);
+var material  = new THREE.ShaderMaterial({
+    uniforms: uniforms,
     vertexShader: textVShader,
     fragmentShader: textFShader
 });
-material2.map    = THREE.ImageUtils.loadTexture('./ocean_dist.png')
-var earthMesh = new THREE.Mesh(geometry2, material2);
+
+var earthMesh = new THREE.Mesh(geometry, material);
 scene.add(earthMesh);
-window.mesh = earthMesh;
-
-// add subtle blue ambient lighting
-var ambientLight = new THREE.AmbientLight(0x444499);
-scene.add(ambientLight);
-
-// directional lighting
-var directionalLight = new THREE.DirectionalLight(0xffffff);
-directionalLight.position.set(1, 1, 1).normalize();
-scene.add(directionalLight);
 
 var startTime = new Date();
 var dt = 0.0;
@@ -86,19 +52,16 @@ var dt = 0.0;
 function tick() {
     var now = new Date();
     dt = (now - startTime) / 1000.0;
-    uniforms.time.value = Math.PI/2;//dt/3;
+    uniforms.time.value = dt;
 }
 
 function render() {
-    earthMesh.rotation.y = dt / 4.0;
     renderer.render(scene, camera);
 }
 
 function step(timestamp) {
-    stats.begin();
     tick();
     render();
-    stats.end();
     requestAnimationFrame(step);
 }
 
